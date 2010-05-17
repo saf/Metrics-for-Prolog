7a78bd7dfe0c61ef53b6cd1f2140ce3f02483ca7
saf <>
2010-05-17 22:10:40 +0200

    Article: introduction to Prolog language

---
+++ local.pl
@@ -0,0 +1,7 @@
+% Markusz & Kaposi's local complexity measure for Prolog.
+
+:- module(local, [local_analyse/1]).
+
+% local_analyse(+Terms)
+%   Perform local complexity analysis of Terms
+local_analyse(Terms).
