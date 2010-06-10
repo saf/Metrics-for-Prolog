aec9b70f3e15404ec0643e20dedfa36866ed81d6
saf <saf@aleph.(none)>
2010-06-10 18:43:34 +0200

    HTML_vis refactoring
    Refactoring of HTML_vis. Created a few procedures to clear things up.

--- html_vis.pm
+++ html_vis.pm
@@ -4,6 +4,7 @@ package html_vis;
 
 use Data::Dumper;
 use GraphViz;
+use utf8;
 
 use html;
 use util;
@@ -14,7 +15,7 @@ return 1;
 sub visualise($) {
     my ($data) = @_;
     
-    my $deps = get_dependencies($data);
+    my $deps = util::get_dependencies($data);
     create_dep_graph($deps);
     project_summary($data, $deps);    
 
@@ -25,18 +26,68 @@ sub visualise($) {
 
 sub project_summary($) {
     my ($data, $deps) = @_;
+    my $summary = util::project_overall_info($data, $deps);
 
     open OVERALL, ">output/index.html";
     html::header(\*OVERALL, "Metrics for Prolog");
 
-    my $pkgs = util::get_packages($data);
-    my $number_packages = @$pkgs;
-    my $summary = project_overall_info($data, $deps);
-    my $compl_rating = rate::local_complexity($summary->{predicates}->{ave_complexity});
-
     print OVERALL <<_END;
         <h2>Project metrics summary</h2>
 	<div class="separator"></div>
+_END
+
+    print_project_summary(\*OVERALL, $summary); 
+    print_dependency_graph(\*OVERALL);
+    print_package_list(\*OVERALL, $data, $deps);
+    
+    html::footer(\*OVERALL);
+}
+
+sub create_dep_graph($) {
+    my ($deps) = @_;
+    
+    my $gv = GraphViz->new();
+    for (keys %{$deps->{outgoing}}) {
+	$gv->add_node($_);
+    };
+    for my $pkg (keys %{$deps->{outgoing}}) {
+	for (@{$deps->{outgoing}->{$pkg}}) {
+	    $gv->add_edge($pkg => $_);
+	}
+    };
+
+    open GRAPH, ">output/packages.png";
+    print GRAPH $gv->as_png;
+    close GRAPH;
+}
+
+sub package_details($$$) {
+    my ($pkg, $data, $deps) = @_;
+    
+    my $out = package_page_name($pkg);
+    my $preds = pl_package::get_predicates($data);
+    my @predicates = sort { $a->{name} cmp $b->{name} } @$preds;
+
+    open OUTPUT, ">output/$out";
+    html::header(\*OUTPUT, "Package $pkg");
+    print OUTPUT <<_END;
+    <a class="backLink" href="index.html">Back to package list</a>
+    <h2>Package <span class="packageName">$pkg</span></h2>
+    <div class="separator"></div>	
+_END
+
+    print_package_summary(\*OUTPUT, $pkg, $data, $deps);
+    print_predicate_list(\*OUTPUT, \@predicates);
+    html::footer(\*OUTPUT);
+}
+
+# Output element printing
+
+sub print_project_summary($$) {
+    my ($out, $summary) = @_;
+    my $compl_rating = rate::local_complexity($summary->{predicates}->{ave_complexity});
+
+    print $out <<_END;
 	<h3>Project summary</h3>
 	  <table class="summaryTable">
 	    <tr><td colspan="2" class="hdr">Code metrics</th></tr>
@@ -64,11 +115,25 @@ sub project_summary($) {
                   <span style="color: $compl_rating->{color}">($compl_rating->{long})</span>
               </td></tr>
 	  </table>
+_END
+}
 	  
+sub print_dependency_graph($) {
+    my ($out) = @_;
+
+    print $out <<_END;
 	<h3>Package dependency graph</h3>
 	  <p class="explanation">The following graph shows relationships between the project modules. 
 	    An arrow from package X to package Y means that package X consults Y. </p>
 	    <img src="packages.png" />
+_END
+}
+
+sub print_package_list($$$) {
+    my ($out, $data, $deps) = @_;
+    my $pkgs = util::get_packages($data);
+
+    print $out <<_END;
 	<h3>List of packages</h3>
 	  <p class="explanation">The list contains all Prolog files included within the project, along
 	    with some top-level statistics of the files. Click on the file name to view details on
@@ -84,15 +149,30 @@ sub project_summary($) {
 	    <th colspan="2">Average LC</th>
 	  </tr>
 _END
-    my $trClass = "odd";
-    for my $pkg (sort @$pkgs) {
-	my $pkgpage = package_page_name($pkg);
-	my $info = package_overall_info($pkg, $data->{$pkg}, $deps);
-	my $complexity_rating = rate::local_complexity($info->{average_complexity});
-	my $volume_rating = rate::package_volume($info->{total_volume});
-	my $effort_rating = rate::package_effort($info->{total_effort});
 
-	print OVERALL <<_END;
+    my $even = 0;
+    for my $pkg (sort @$pkgs) {
+	print_package_row($out, $pkg, $data, $deps, $even);
+        $even = $even ? 0 : 1;
+    };
+    
+    print $out <<_END;
+        </table>
+_END
+}
+
+
+sub print_package_row($$$$;$) {
+    my ($out, $pkg, $data, $deps, $even) = @_;
+
+    my $pkgpage = package_page_name($pkg);
+    my $info = util::package_overall_info($pkg, $data->{$pkg}, $deps);
+    my $complexity_rating = rate::local_complexity($info->{average_complexity});
+    my $volume_rating = rate::package_volume($info->{total_volume});
+    my $effort_rating = rate::package_effort($info->{total_effort});
+    my $trClass = $even ? 'even' : 'odd';
+
+    print $out <<_END;
 	  <tr class="$trClass">
 	      <td class="mainCell lastInGroup"><a href="$pkgpage">$pkg</a></td>
 	      <td>$info->{fan_in}</td>
@@ -108,119 +188,18 @@ _END
               <td class="left" style="color: $complexity_rating->{color}">($complexity_rating->{long})</td>
 	  </tr>
 _END
-        $trClass = ($trClass eq 'odd') ? 'even' : 'odd';
-    };
-    
-    print OVERALL <<_END;
-        </table>
-_END
-
-    html::footer(\*OVERALL);
 }
 
-sub package_overall_info($$$) {
-    my ($pkg, $data, $deps) = @_;
+sub print_package_summary($$$$) {
+    my ($out, $pkg, $data, $deps) = @_;
 
-    my $fanin  = @{$deps->{incoming}->{$pkg}};
-    my $fanout = @{$deps->{outgoing}->{$pkg}};
-
-    my $predicates = pl_package::get_predicates($data);
-    my $npred      = @$predicates;
-    my $total_volume = 0;
-    my $total_effort = 0;
-    my $total_complexity = 0;
-    my $total_partitions = 0;
-    
-    for (@$predicates) {
-	$total_volume += $_->{halstead}->{volume}; 
-	$total_effort += $_->{halstead}->{effort};
-	$total_complexity += $_->{local}->{sum};
-	$total_partitions += $_->{local}->{n_partitions};
-    };
-
-    my $average_complexity = $total_partitions == 0 ? 0 : $total_complexity / $total_partitions;
-
-    return { 
-	fan_in => $fanin, 
-	fan_out => $fanout, 
-	n_predicates => $npred,
-	total_volume => $total_volume,
-	total_effort => $total_effort, 
-	average_complexity => $average_complexity,
-    };
-}
-
-sub get_dependencies($) {
-    my ($data) = @_;
-    my $outgoing = {};
-    my $incoming = {};
-    my $pkgs = util::get_packages($data);
-
-    for my $pkg (@$pkgs) {
-	$incoming->{$pkg} = [];
-	$outgoing->{$pkg} = [];
-    };
-
-    for my $pkg (sort @$pkgs) {
-	my $links = $data->{$pkg}->{links};
-
-	if ($links =~ /^ARRAY/) {
-	    for (@$links) {
-		my $dep = $_->{ref};
-		push @{$outgoing->{$pkg}}, $dep;
-		push @{$incoming->{$dep}}, $pkg;
-	    }
-	}
-    }
-
-    return { 
-	incoming => $incoming, 
-	outgoing => $outgoing,
-    };
-}
-
-sub create_dep_graph($) {
-    my ($deps) = @_;
-    
-    my $gv = GraphViz->new();
-    for (keys %{$deps->{outgoing}}) {
-	$gv->add_node($_);
-    };
-    for my $pkg (keys %{$deps->{outgoing}}) {
-	for (@{$deps->{outgoing}->{$pkg}}) {
-	    $gv->add_edge($pkg => $_);
-	}
-    };
-
-    open GRAPH, ">output/packages.png";
-    print GRAPH $gv->as_png;
-    close GRAPH;
-}
-
-sub package_details($$$) {
-    my ($pkg, $data, $deps) = @_;
-    
-    my $out = package_page_name($pkg);
-
-    open OUTPUT, ">output/$out";
-    html::header(\*OUTPUT, "Package $pkg");
-
-    my $details = package_info($pkg, $data, $deps);
+    my $details = util::package_info($pkg, $data, $deps);
     my $complexity_rating = rate::local_complexity($details->{complexity}->{average_complexity});
     my $volume_rating = rate::package_volume($details->{halstead}->{volume});
     my $effort_rating = rate::package_effort($details->{halstead}->{effort});
-				     
-    my $preds = pl_package::get_predicates($data);
-    my @predicates = sort { $a->{name} cmp $b->{name} } @$preds;
-    my @incoming = @{$deps->{incoming}->{$pkg}};
-    my @outgoing = @{$deps->{outgoing}->{$pkg}};
 
-    print OUTPUT <<_END;
-    <a class="backLink" href="index.html">Back to package list</a>
-    <h2>Package <span class="packageName">$pkg</span></h2>
-    <div class="separator"></div>	
-	
-    <h3>Package summary</h3>
+    print $out <<_END;
+     <h3>Package summary</h3>
 
        <table class="summaryTable">
          <tr><td colspan="2" class="hdr">Code metrics</th></tr>
@@ -250,12 +229,29 @@ sub package_details($$$) {
 
        <table class="summaryTable">
 	 <tr><td colspan="2" class="hdr">
-	    <a class="detailsShowHideLink" href="#">Show details &gt;&gt;</a>
+	    <a class="detailsShowHideLink">Show details &gt;&gt;</a>
 	    Package coupling metrics</th></tr>
 	 <tr><th>Fan-in</th><td>$details->{relationships}->{fan_in}</td></tr>
 	 <tr><th>Fan-out</th><td>$details->{relationships}->{fan_out}</td></tr>
 	 <tr id="couplingDetails"><td colspan="2" class="detailsHolder">
- 	    <table class="hiddenDetails">
+_END
+
+    print_links($out, $pkg, $deps);
+
+    print $out <<_END;
+         </td></tr>
+       </table>
+_END
+}
+
+sub print_links($$$) {
+    my ($out, $pkg, $deps) = @_;
+				     
+    my @incoming = @{$deps->{incoming}->{$pkg}};
+    my @outgoing = @{$deps->{outgoing}->{$pkg}};
+
+    print $out <<_END;
+ 	  <table class="hiddenDetails">
 	    <tr>
 	       <th class="inSummaryDetailsHdr">Consulted by:</th>
 	       <th class="inSummaryDetailsHdr">Consults:</th>
@@ -277,17 +273,22 @@ _END
 	    $outLink = "<a href=\"$page\">$outgoing[$i]</a>";
 	};
 	$i++;
-	print OUTPUT <<_END;
+	print $out <<_END;
 	    <tr>
 		<td>$incLink</td>
 		<td>$outLink</td>
 	    </tr>		
 _END
     }
-    print OUTPUT <<_END;
-	    </table></tr></td>
-       </table>
+    print $out <<_END;
+	    </table>
+_END
+}
 
+sub print_predicate_list($$) {
+    my ($out, $predicates) = @_;
+
+    print $out <<_END;
     <h3>Predicates</h3>
 
     Click on a predicate name to view details on its clauses.
@@ -307,21 +308,35 @@ _END
     </tr>    
 _END
 
-    my $trClass = "odd";
     my $pn = 0;
-    for my $p (@predicates) {
-	my $clauses = $p->{local}->{partitions};
-	my $n_clauses = @$clauses;
-	my $est_time = ${format_time($p->{halstead}->{time})};
-	my $av_compl_rating = rate::local_complexity($p->{local}->{average});
-	my $mx_compl_rating = rate::local_complexity($p->{local}->{max});
-	my $t_compl_rating  = rate::total_complexity($p->{local}->{sum});
-	my $diff_rating = rate::difficulty($p->{halstead}->{difficulty});
-	my $effort_rating = rate::predicate_effort($p->{halstead}->{effort});
-	my $volume_rating = rate::predicate_volume($p->{halstead}->{volume});
+    for my $p (@$predicates) {
+	print_predicate_row($out, $p, $pn);
+	$pn++;
+    }
 
-        print OUTPUT <<_END;
-    <tr class="$trClass">
+    print $out <<_END;
+    </table>
+_END
+}
+
+
+sub print_predicate_row($$$) {
+    my ($out, $p, $pn) = @_;
+
+    my $trClass = $pn % 2 ? 'odd' : 'even';
+
+    my $clauses = $p->{local}->{partitions};
+    my $n_clauses = @$clauses;
+    my $est_time = ${format_time($p->{halstead}->{time})};
+    my $av_compl_rating = rate::local_complexity($p->{local}->{average});
+    my $mx_compl_rating = rate::local_complexity($p->{local}->{max});
+    my $t_compl_rating  = rate::total_complexity($p->{local}->{sum});
+    my $diff_rating = rate::difficulty($p->{halstead}->{difficulty});
+    my $effort_rating = rate::predicate_effort($p->{halstead}->{effort});
+    my $volume_rating = rate::predicate_volume($p->{halstead}->{volume});
+
+    print $out <<_END;
+      <tr class="$trClass">
 	<td class="mainCell lastInGroup"><a class="cdShowHide" cdid="$pn">$p->{name}</a></td>
 	<td class="lastInGroup">$n_clauses</td>
 	<td>$p->{halstead}->{length}</td>
@@ -337,147 +352,70 @@ _END
 	<td class="left" style="color: $mx_compl_rating->{color}"
 	    title="$mx_compl_rating->{long}">($mx_compl_rating->{short})</td>
 	<td style="color: $t_compl_rating->{color}">$p->{local}->{sum}</td>
-    </tr>
-    <tr><td colspan="13" class="detailsHolder">
-      <div class="clausesDetails" id="cd_$pn">
-        <table class="detailsTable">
-	   <tr class="mainHdr">
-	     <th rowspan="2" class="lastInGroup">#</td>
-	     <th rowspan="2" class="lastInGroup">Type</td>
-	     <th colspan="4" class="lastInGroup">Local complexity details</th>
-	     <th rowspan="2" colspan="2">Complexity</th>
-	   </tr>
-	   <tr class="subHdr">
-	     <th>Sub</th>
-	     <th>Dat</th>
-	     <th>Var</th>
-	     <th class="lastInGroup">Rel</th>
-	   </tr>
+      </tr>
+      <tr><td colspan="13" class="detailsHolder">
 _END
-        my $inTrClass = 'odd';
-        for my $c (@$clauses) {
-	    my $compl = $c->{subproblems} + $c->{new_entities} + $c->{new_variables} + $c->{relations_complexity};
-	    my $compl_rating = rate::local_complexity($compl);
-	    print OUTPUT <<_END;
-	    <tr class="$inTrClass">
-		<td class="lastInGroup">${\($c->{id}+1)}</td>
-		<td class="lastInGroup">$c->{type}</td>
-		<td>$c->{subproblems}</td>
-		<td>$c->{new_entities}</td>
-		<td>$c->{new_variables}</td>
-		<td class="lastInGroup">$c->{relations_complexity}</td>
-		<td class="right">$compl</td><td class="left" style="color: $compl_rating->{color}">($compl_rating->{long})</td>
+
+      print_clauses_list($out, $clauses, $pn);
+
+    print $out <<_END;
+      </td></tr>
+_END
+}
+
+sub print_clauses_list($$$) {
+    my ($out, $clauses, $pn) = @_;
+
+    print $out <<_END;
+	<div class="clausesDetails" id="cd_$pn">
+          <table class="detailsTable">
+	    <tr class="mainHdr">
+	      <th rowspan="2" class="lastInGroup">#</td>
+	      <th rowspan="2" class="lastInGroup">Type</td>
+	      <th colspan="4" class="lastInGroup">Local complexity details</th>
+	      <th rowspan="2" colspan="2">Complexity</th>
+	    </tr>
+	    <tr class="subHdr">
+	      <th>Sub</th>
+	      <th>Dat</th>
+	      <th>Var</th>
+	      <th class="lastInGroup">Rel</th>
 	    </tr>
 _END
-            $inTrClass = ($inTrClass eq 'odd') ? 'even' : 'odd';
-        }
-	print OUTPUT <<_END;
-	</table>
-    </td></tr>
-_END
-        $trClass = ($trClass eq 'even') ? 'odd' : 'even';
-	$pn++;
-    }
 
-    print OUTPUT <<_END;
-    </table>
-_END
-    html::footer(\*OUTPUT);
-}
-
-sub project_overall_info($) {
-    my ($data, $deps) = @_;
-
-    my $loc = 0;
-    my $eloc = 0;
-    my $cloc = 0;
-    my $total_effort = 0;
-    my $total_volume = 0;
-    my $total_time = 0;
-    my $total_complexity = 0;
-    my $total_predicates = 0;
-    my $links = 0;
-
-    my $pkgs = util::get_packages($data);
-    my $n_packages = @$pkgs;
-    for my $pkg (@$pkgs) {
-	my $info = package_info($pkg, $data->{$pkg}, $deps);
-	
-	$loc += $info->{code}->{loc}, 
-	$eloc += $info->{code}->{effective_loc},
-	$cloc += $info->{code}->{loc_comments}, 
-
-	$total_effort += $info->{halstead}->{effort};
-	$total_volume += $info->{halstead}->{volume};
-	$total_predicates += $info->{predicates}->{number};
-	$total_complexity += $info->{complexity}->{average_complexity} * $info->{predicates}->{number};
-	$total_time += $info->{halstead}->{time};
-
-	$links += $info->{relationships}->{fan_in};
-    }
-
-    return { 
-	code => {
-	    loc => $loc, 
-	    eloc => $eloc,
-	    comments_per_line => $eloc == 0 ? 0 : $cloc / $eloc,
-	}, 
-	packages => {
-	    number => $n_packages, 
-	    loc_per_package => $n_packages == 0 ? 0 : $loc / $n_packages,
-	}, 
-	halstead => {
-	    volume => $total_volume, 
-	    effort => $total_effort, 
-	    time => $total_time * (log($links+1) / log(2)),
-	}, 
-	predicates => {
-	    number => $total_predicates, 
-	    ave_loc => $total_predicates == 0 ? 0 : $loc / $total_predicates, 
-	    ave_complexity => $total_predicates == 0 ? 0 : $total_complexity / $total_predicates,
-	},
+    my $even = 0;
+    for my $c (@$clauses) {
+	print_clause_row($out, $c, $even);
+	$even = $even ? 0 : 1;
     };
+
+    print $out <<_END;
+          </table>
+_END
 }
 
-sub package_info($$$) {
-    my ($pkg, $data, $deps) = @_;
+sub print_clause_row($$;$) {
+    my ($out, $c, $even) = @_;
 
-    my $predicates = pl_package::get_predicates($data);
-    my $overall = package_overall_info($pkg, $data, $deps); 
-    my $n_predicates = $overall->{n_predicates};
+    my $compl = $c->{subproblems} + $c->{new_entities} + $c->{new_variables} + $c->{relations_complexity};
+    my $compl_rating = rate::local_complexity($compl);
+    my $trClass = $even ? 'even' : 'odd';
 
-    return {
-	code => {
-	    loc => $data->{loc},
-	    effective_loc => $data->{effective_loc}, 
-	    loc_comments => $data->{comments}, 
-	    comments_per_line => $data->{effective_loc} == 0 
-		? 0 
-		: $data->{comments} / $data->{effective_loc},
-	},
-	predicates => {
-	    number => $n_predicates, 
-	    loc_per_predicate => $n_predicates == 0 
-		? 0 
-		: $data->{effective_loc} / $n_predicates,
-	    comments_per_predicate => $n_predicates == 0 
-	        ? 0 
-		: $data->{comments} / $n_predicates,
-	},
-	halstead => {
-	    volume => $overall->{total_volume}, 
-	    effort => $overall->{total_effort}, 
-	    time   => $overall->{total_effort} / 10 * (log($n_predicates + 1) / log(2)),
-        },
-	complexity => {
-	    average_complexity => $overall->{average_complexity}, 
-	},
-	relationships => {
-	    fan_in => $overall->{fan_in}, 
-	    fan_out => $overall->{fan_out}, 
-	},
-    };
+    print $out <<_END;
+	<tr class="$trClass">
+	    <td class="lastInGroup">${\($c->{id}+1)}</td>
+	    <td class="lastInGroup">$c->{type}</td>
+	    <td>$c->{subproblems}</td>
+	    <td>$c->{new_entities}</td>
+	    <td>$c->{new_variables}</td>
+	    <td class="lastInGroup">$c->{relations_complexity}</td>
+	    <td class="right">$compl</td><td class="left" style="color: $compl_rating->{color}">($compl_rating->{long})</td>
+	</tr>
+_END
 }
+
+
+# Utility functions
 
 sub package_page_name($) {
     my ($name) = @_;
--- util.pm
+++ util.pm
@@ -119,3 +119,160 @@ sub get_packages($) {
     my ($data) = @_;
     return [keys %$data];
 }
+
+sub project_overall_info($$) {
+    my ($data, $deps) = @_;
+
+    my $loc = 0;
+    my $eloc = 0;
+    my $cloc = 0;
+    my $total_effort = 0;
+    my $total_volume = 0;
+    my $total_time = 0;
+    my $total_complexity = 0;
+    my $total_predicates = 0;
+    my $links = 0;
+
+    my $pkgs = util::get_packages($data);
+    my $n_packages = @$pkgs;
+    for my $pkg (@$pkgs) {
+	my $info = package_info($pkg, $data->{$pkg}, $deps);
+	
+	$loc += $info->{code}->{loc}, 
+	$eloc += $info->{code}->{effective_loc},
+	$cloc += $info->{code}->{loc_comments}, 
+
+	$total_effort += $info->{halstead}->{effort};
+	$total_volume += $info->{halstead}->{volume};
+	$total_predicates += $info->{predicates}->{number};
+	$total_complexity += $info->{complexity}->{average_complexity} * $info->{predicates}->{number};
+	$total_time += $info->{halstead}->{time};
+
+	$links += $info->{relationships}->{fan_in};
+    }
+
+    return { 
+	code => {
+	    loc => $loc, 
+	    eloc => $eloc,
+	    comments_per_line => $eloc == 0 ? 0 : $cloc / $eloc,
+	}, 
+	packages => {
+	    number => $n_packages, 
+	    loc_per_package => $n_packages == 0 ? 0 : $loc / $n_packages,
+	}, 
+	halstead => {
+	    volume => $total_volume, 
+	    effort => $total_effort, 
+	    time => $total_time * (log($links+1) / log(2)),
+	}, 
+	predicates => {
+	    number => $total_predicates, 
+	    ave_loc => $total_predicates == 0 ? 0 : $loc / $total_predicates, 
+	    ave_complexity => $total_predicates == 0 ? 0 : $total_complexity / $total_predicates,
+	},
+    };
+}
+
+sub package_info($$$) {
+    my ($pkg, $data, $deps) = @_;
+
+    my $predicates = pl_package::get_predicates($data);
+    my $overall = package_overall_info($pkg, $data, $deps); 
+    my $n_predicates = $overall->{n_predicates};
+
+    return {
+	code => {
+	    loc => $data->{loc},
+	    effective_loc => $data->{effective_loc}, 
+	    loc_comments => $data->{comments}, 
+	    comments_per_line => $data->{effective_loc} == 0 
+		? 0 
+		: $data->{comments} / $data->{effective_loc},
+	},
+	predicates => {
+	    number => $n_predicates, 
+	    loc_per_predicate => $n_predicates == 0 
+		? 0 
+		: $data->{effective_loc} / $n_predicates,
+	    comments_per_predicate => $n_predicates == 0 
+	        ? 0 
+		: $data->{comments} / $n_predicates,
+	},
+	halstead => {
+	    volume => $overall->{total_volume}, 
+	    effort => $overall->{total_effort}, 
+	    time   => $overall->{total_effort} / 10 * (log($n_predicates + 1) / log(2)),
+        },
+	complexity => {
+	    average_complexity => $overall->{average_complexity}, 
+	},
+	relationships => {
+	    fan_in => $overall->{fan_in}, 
+	    fan_out => $overall->{fan_out}, 
+	},
+    };
+}
+
+
+
+sub package_overall_info($$$) {
+    my ($pkg, $data, $deps) = @_;
+
+    my $fanin  = @{$deps->{incoming}->{$pkg}};
+    my $fanout = @{$deps->{outgoing}->{$pkg}};
+
+    my $predicates = pl_package::get_predicates($data);
+    my $npred      = @$predicates;
+    my $total_volume = 0;
+    my $total_effort = 0;
+    my $total_complexity = 0;
+    my $total_partitions = 0;
+    
+    for (@$predicates) {
+	$total_volume += $_->{halstead}->{volume}; 
+	$total_effort += $_->{halstead}->{effort};
+	$total_complexity += $_->{local}->{sum};
+	$total_partitions += $_->{local}->{n_partitions};
+    };
+
+    my $average_complexity = $total_partitions == 0 ? 0 : $total_complexity / $total_partitions;
+
+    return { 
+	fan_in => $fanin, 
+	fan_out => $fanout, 
+	n_predicates => $npred,
+	total_volume => $total_volume,
+	total_effort => $total_effort, 
+	average_complexity => $average_complexity,
+    };
+}
+
+sub get_dependencies($) {
+    my ($data) = @_;
+    my $outgoing = {};
+    my $incoming = {};
+    my $pkgs = util::get_packages($data);
+
+    for my $pkg (@$pkgs) {
+	$incoming->{$pkg} = [];
+	$outgoing->{$pkg} = [];
+    };
+
+    for my $pkg (sort @$pkgs) {
+	my $links = $data->{$pkg}->{links};
+
+	if ($links =~ /^ARRAY/) {
+	    for (@$links) {
+		my $dep = $_->{ref};
+		push @{$outgoing->{$pkg}}, $dep;
+		push @{$incoming->{$dep}}, $pkg;
+	    }
+	}
+    }
+
+    return { 
+	incoming => $incoming, 
+	outgoing => $outgoing,
+    };
+}
